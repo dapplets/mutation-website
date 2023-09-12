ec62b535fe9eb26286690379e64eadd50c6ef059
Alexander Sakhaev <alsakhaev@gmail.com>
2023-09-12 15:29:04 +0200

    refactor: remove unused deps

--- src/pages/MutablePage/index.tsx
+++ src/pages/MutablePage/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react'
+import { FC } from 'react'
 import styles from './MutablePage.module.scss'
 import Download from '../../assets/images/icons/download.svg'
 import { useSearchParams } from 'react-router-dom'
